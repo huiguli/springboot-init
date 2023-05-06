8dfa5eee4ec2f7ec6c7c141b0845bc562312b913
huiguli <847819010@qq.com>
2023-05-06 22:59:58 +0800

    springboot init

---
+++ src/main/java/com/huiguli/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.huiguli.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huiguli.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
